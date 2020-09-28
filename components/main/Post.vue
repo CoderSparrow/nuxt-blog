75f18f32a07f1be7eba8185ccf73d422de2a0770
Savchuk <savchuk-sergej@smartworld.team>
2020-09-28 11:55:34 +0700

    create Post component

---
+++ components/main/Post.vue
@@ -0,0 +1,18 @@
+<template>
+  <article class="post">
+  </article>
+</template>
+
+<script>
+export default {
+  name: "Post"
+}
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    height: 200px;
+    margin-bottom: 2rem;
+    border: 1px solid #000;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <div></div>
+  <section>
+    <el-row type="flex" justify="center">
+      <el-col :xs="24" :sm="18" :md="12" :lg="10">
+        <Post v-for="post in 5"/>
+      </el-col>
+    </el-row>
+  </section>
 </template>
 
 <script>
-export default {}
+import Post from "@/components/main/Post";
+
+export default {
+  head: {
+    title: 'Главная'
+  },
+  components: {
+    Post
+  }
+}
 </script>
 
 <style>
